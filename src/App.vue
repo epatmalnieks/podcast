fe0af6f933d21ee0470a8d3496742f5bcd831096
epatmalnieks <epatmalnieks04@yahoo.com>
2019-01-21 17:22:43 -0500

    Add media queries

--- src/App.vue
+++ src/App.vue
@@ -13,4 +13,39 @@
   body {
     font-family: 'Raleway-Regular';
   }
+
+  // Extra small devices (portrait phones, less than 576px)
+@media (max-width: 575.98px) {
+  body {
+    background-color: blue;
+  }
+}
+
+// Small devices (landscape phones, 576px and up)
+@media (min-width: 576px) and (max-width: 767.98px) {
+  body {
+    background-color: red;
+  }
+  }
+
+// Medium devices (tablets, 768px and up)
+@media (min-width: 768px) and (max-width: 991.98px) {
+  body {
+    background-color: yellow;
+  }
+ }
+
+// Large devices (desktops, 992px and up)
+@media (min-width: 992px) and (max-width: 1199.98px) {
+  body {
+    background-color: green;
+  }
+ }
+
+// Extra large devices (large desktops, 1200px and up)
+@media (min-width: 1200px) {
+  body {
+    background-color: pink;
+  }
+ }
 </style>
