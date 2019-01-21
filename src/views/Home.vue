44c99ab684e7677d3c0b42e02250e1916fe3d2d6
epatmalnieks <epatmalnieks04@yahoo.com>
2019-01-21 12:54:16 -0500

    Add fake list of recents

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,47 @@
 <template>
+<div>
   <div>
     <button @click='goToBios'>Bios</button>
   </div>
+  <div>
+    <p>Recent Podcasts</p>
+    <div v-for='(podcast, key) in recentPodcasts' :key='key'>
+    <p>{{podcast.title}}</p>
+    <p>{{podcast.date}}</p>
+    <p>{{podcast.summary}}</p>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
   name: 'home',
+  data() {
+    return {
+      recentPodcasts: [{
+        title: 'Podcast 1',
+        date: '1/21/2019',
+        summary: 'Summary 1',
+      }, {
+        title: 'Podcast 2',
+        date: '1/22/2019',
+        summary: 'Summary 2',
+      }, {
+        title: 'Podcast 3',
+        date: '1/23/2019',
+        summary: 'Summary 3',
+      }, {
+        title: 'Podcast 4',
+        date: '1/24/2019',
+        summary: 'Summary 4',
+      }, {
+        title: 'Podcast 5',
+        date: '1/25/2019',
+        summary: 'Summary 5',
+      }],
+    };
+  },
   methods: {
     goToBios() {
       this.$router.push({ name: 'bios' });
