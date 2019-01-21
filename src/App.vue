a3d5721d957e39b14791fd82120f2bd3a5295648
epatmalnieks <epatmalnieks04@yahoo.com>
2019-01-21 12:14:59 -0500

    Set up bios route

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
 <template>
   <div id="app">
-    <p>Erik and Ryan's podcast, coming soon.</p>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="home">
+  <div>
+    <button @click='goToBios'>Bios</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'home',
+  methods: {
+    goToBios() {
+      this.$router.push({ name: 'bios' });
+    },
+  },
 };
 </script>
