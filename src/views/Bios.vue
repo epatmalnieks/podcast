016031faa56a42a27b2e11695c73a5e36bb72a6e
epatmalnieks <epatmalnieks04@yahoo.com>
2019-01-30 22:01:01 -0500

    Add temporary profile pictures

--- src/views/Bios.vue
+++ src/views/Bios.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <h2>Bios</h2>
-    <p class="erik">Erik Patmalnieks is a software developer by day, and a sports aficionado by night. Erik
+    <p class="erik">
+      <img class="biosImage" src="../assets/images/erik.jpg"/>
+      Erik Patmalnieks is a software developer by day, and a sports aficionado by night. Erik
       works for General Motors in Detroit, Michigan building software for
       infotainment systems in vehicles. He has a Bachelor's Degree in Computer Science
       with a major in Software Engineering from the University of Michigan. In his
       free time when not watching sports, he bowls in a league and plays a hefty amount of video games.
     </p>
-    <p class="ryan">Ryan Belous is a content blue collar team leader in the steel working industry,
+    <p class="ryan">
+    <img class="biosImage" src="../assets/images/ryan.jpg"/>
+    Ryan Belous is a content blue collar team leader in the steel working industry,
     and professional actor at Pornhub. Previously, Ryan worked as a petty officer and
     is a veteran of the United States Navy.
     Outside of work, Ryan enjoys spending time with his beautiful wife and daughter.
