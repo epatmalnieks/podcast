7fa1e2fb06b439f16c772b2a82524c2a286941d3
epatmalnieks <epatmalnieks04@yahoo.com>
2019-02-05 22:28:48 -0500

    Add test coverage

--- vue.config.js
+++ vue.config.js
@@ -7,4 +7,19 @@ module.exports = {
       patterns: [path.resolve(__dirname, './src/styles/global.scss')],
     },
   },
+  chainWebpack: (config) => {
+    if (process.env.NODE_ENV !== 'production') {
+      config.devtool('eval');
+      config.module
+        .rule('istanbul')
+        .test(/\.(js|vue)$/)
+        .enforce('post')
+        .include
+        .add(path.resolve(__dirname, '/src'))
+        .end()
+        .use('istanbul-instrumenter-loader')
+        .loader('istanbul-instrumenter-loader')
+        .options({ esModules: true });
+    }
+  },
 };
