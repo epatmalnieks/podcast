5880fe190f9b67cc73149fd14ea6e885ef07907d
epatmalnieks <epatmalnieks04@yahoo.com>
2019-05-01 21:10:09 -0400

    Make slogan look nice

--- src/views/App.vue
+++ src/views/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <nav-component></nav-component>
     <div class="slogan">
-      <p>A podcast with 78% sports and 22% nonsense.</p>
+      <p class="sloganMessage">A podcast with 78% sports and 22% nonsense.</p>
     </div>
     <h1 style="display:none">78% Sports</h1>
     <img class="logo" alt="78% Sports" src="../assets/images/78percentsports.png"/>
