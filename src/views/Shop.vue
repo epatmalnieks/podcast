85c494d4ac29edc9fdb6e08bf6dffba03e7b6875
epatmalnieks <epatmalnieks04@yahoo.com>
2019-05-02 19:11:30 -0400

    Change wording about sizes

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -4,7 +4,7 @@
   <p>
     Check out our entire <a class="amazon" href="https://www.amazon.com/s?rh=n%3A7141123011%2Cp_4%3A78%25+Sports+Apparel&fbclid=IwAR0wzEkeeiB5Bk9v3EC8hC0nGeH4E2zxyVSxqhAttm0nSFCPIq5Q090Nuf8&ref=w_bl_sl_s_ap_web_7141123011">Amazon store</a>, or
     select an item below!
-    Each item has multiple colors to choose from. Sizes run a little small, so you may want to order one size bigger. They usually take about 6 days from being printed to arriving at your doorstep.
+    Each item has multiple colors to choose from. T-Shirt sizes run a little small, so you may want to order one size bigger. All other items should fit normally. They usually take about 6 days from being printed to arriving at your doorstep.
   </p>
   <p class="thankYou">
     Thank you for your support!
