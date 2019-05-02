66c07b44a30f7543f7becea5cb60443184da9954
epatmalnieks <epatmalnieks04@yahoo.com>
2019-05-01 21:35:06 -0400

    Add contact us info

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h2>Contact Us</h2>
+    <h3>Facebook:</h3>
+    <a class="links" href="https://www.facebook.com/78percentsports">https://www.facebook.com/78percentsports</a>
+    <h3>E-mail:</h3>
+    <a class="links" href="mailto:78percentsports@gmail.com">78percentsports@gmail.com</a>
   </div>
 </template>
 
